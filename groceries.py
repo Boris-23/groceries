91b5164a7f69c0daebbdb344a0e484783800124a
Adrian Perez <adrian6@pdx.edu>
2024-10-07 00:50:54 -0700

    Fixing main

--- groceries.py
+++ groceries.py
@@ -1,11 +1,19 @@
-print('\t====== Hello =====')
-print('This is creating a list of groceries\n')
-print('for the week.  The project is a work in progress\n')
-print('and it will be finalized when it is completed.\n')
-print('\t==================')
+import sys
+
+print("test")
+def welcome():
+    print("\t====== Hello =====")
+    print('This is creating a list of groceries\n')
+    print('for the week.  The project is a work in progress\n')
+    print('and it will be finalized when it is completed.\n')
+    print('\t==================')
+
+def main():
+    print("test2")
 
 
-
-
-
- 
+if __name__=="__main__":
+    result = welcome()
+    result2 = main()
+    print(result)
+    print(result2)
