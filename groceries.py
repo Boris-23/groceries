b096dd2b87de49798f0ff14209fbb0f9cbe3aebc
Adrian Perez <adrian6@pdx.edu>
2024-10-07 00:34:49 -0700

    Welcome message and brief description

---
+++ groceries.py
@@ -0,0 +1,11 @@
+print('\t====== Hello =====')
+print('This is creating a list of groceries\n')
+print('for the week.  The project is a work in progress\n')
+print('and it will be finalized when it is completed.\n')
+print('\t==================')
+
+
+
+
+
+ 
