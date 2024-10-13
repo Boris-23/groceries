150dfa8e80a9c799881b1255b49a024664d47b10
Adrian Perez <adrian6@pdx.edu>
2024-10-13 01:45:26 -0700

    update

--- groceries.py
+++ groceries.py
@@ -6,22 +6,30 @@ def welcome():
     print('\t==================')
 
 #This will be the menu for what type of groceries people need 
-def main(choice):
-    choice = int(input('What do you want to choose?'))
-    #list of choices 
-    match choice:
-        case 1:
-               print("Groceries for week")
-        case 2: 
-              print("Writing someone else's groceries")
-        case 3: 
-                print("Template grocery for all cases")
-        case _:
-               print("Incorrect input.  Try again")
+def main(choice, choose):
+
+    #the variable choose is another way of saying yes or no
+    #yes will be 1 and no will be 0
+    choose = int(input('Want to continue? 0 for no and 1 for yes'))
+
+    while choose > 0:
+
+        choice = int(input('What do you want to choose?'))
+        #list of choices 
+        match choice:
+            case 1:
+                print("Groceries for week")
+            case 2: 
+                print("Writing someone else's groceries")
+            case 3: 
+                    print("Template grocery for all cases")
+            case _:
+                print("Incorrect input.  Try again")
 
 
 
 if __name__== "__main__":
     choice = type(int)
+    choose = type(int)
     welcome()
-    main(choice)
+    main(choice, choose)
