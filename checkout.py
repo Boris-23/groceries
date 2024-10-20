4133eaf37d0907c2c546d6af5bba9c5f42807767
Adrian Perez <adrian6@pdx.edu>
2024-10-20 16:13:17 -0700

    Moved majority of code in main.py to checkout.py

--- checkout.py
+++ checkout.py
@@ -14,4 +14,29 @@ import produce as px
 import grains as gx 
 import nuts_seeds as nx
 import dairy as dx
+def welcome():
+    print("\t====== Hello =====")
+    print('This is creating a list of groceries\n')
+    print('for the week.  The project is a work in progress\n')
+    print('and it will be finalized when it is completed.\n')
+    print('\t==================')
+
+#This will be the menu for what type of groceries people need 
+def main(choice):
+        welcome()
+    #the variable choose is another way of saying yes or no
+    #yes will be 1 and no will be 0
+  
+
+        choice = int(input('What do you want to choose?'))
+        #list of choices 
+        match choice:
+            case 1:
+                print("Groceries for week")
+            case 2: 
+                print("Writing someone else's groceries")
+            case 3: 
+                    print("Template grocery for all cases")
+            case _:
+                print("Incorrect input.  Try again")
 
